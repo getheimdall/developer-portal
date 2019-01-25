f70e6448484692e9435b8782d906be4c07936f71
Dijalma Silva <dijalmacz@gmail.com>
2019-01-25 15:10:20 -0300

    Show versions of the Heimdall by api.github

--- pages/updates-api.js
+++ pages/updates-api.js
@@ -1,10 +1,24 @@
+import Moment from 'react-moment'
+import ReactMarkdown from 'react-markdown'
 import Page from './../components/page'
 import SectionPage from './../components/section/SectionPage'
 
-const UpdatesApi = () => (
+const UpdatesApi = ({ releases }) => (
     <Page>
         <SectionPage className="section--last section--top-space" title="Updates history">
             <div>
+                {
+                    releases && 
+                    releases.map((release, index) => {
+                        return <div key={index}>
+                            <h5><strong>Version: </strong>{release.tag_name}</h5>
+                            <h5><strong>Published at: </strong><Moment format="YYYY/MM/DD">{release.created_at}</Moment></h5>
+                            <h5><strong>Features: </strong></h5>
+                            <ReactMarkdown source={release.body} />
+                            <hr/>
+                        </div>
+                    })
+                }
                 <h5><strong>Version: </strong>1.0.0</h5>
                 <h5><strong>Features: </strong></h5>
                 <ul>
@@ -16,5 +30,11 @@ const UpdatesApi = () => (
         </SectionPage>
     </Page>
 )
+
+UpdatesApi.getInitialProps = async () => {
+    const res = await fetch('https://api.github.com/repos/getheimdall/heimdall/releases')
+    const json = await res.json()
+    return { releases: json }
+}
 
 export default UpdatesApi
