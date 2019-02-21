082eb5fb8784e6adddc113695c7b2a265738fd7a
Dijalma Silva <dijalmacz@gmail.com>
2019-02-21 17:24:31 -0300

    Fixed problem to hidden description

--- components/card/Card.js
+++ components/card/Card.js
@@ -16,11 +16,17 @@ class Card extends React.Component {
 
         const { faq, className, id, title, children } = this.props
         const { active } = this.state
+
+        let showDescription = true
+
+        if (faq && !this.state.active) {
+            showDescription = false
+        }
         
         return (
             <div className={`${faq ? 'faq__card' : ''} card ${className}`} id={id}>
                 <h4 onClick={this.toggleCardFaq} className="faq__card-title"> {title} {faq && <span className="faq__card-icon"><i className="mdi mdi-chevron-down"/></span>}</h4>
-                <div className={`${faq ? 'faq__card-description' : ''} ${ !faq || active ? 'active': '' }`}>
+                <div className={`${faq ? 'faq__description' : ''} ${ !faq || active ? 'active': '' }`} style={ showDescription ? { visible : 'visibility'} : { visible : 'hidden'}}>
                     {children}                
                 </div>
             </div>
